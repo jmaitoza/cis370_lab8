978697c2287ec1841024290b73296607a9b5f96b
Joshua Maitoza <jmaitoza@umassd.edu>
2022-03-23 08:28:57 -0400

    started part 3

---
+++ WhatTheShell.c
@@ -0,0 +1,12 @@
+// Joshua Maitoza
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAX_LINE 128 // max length of user command
+
+int main(void) {
+  while (1) {
+    printf("WhatTheShell> "); // Name of shell
+    fflush(stdout);
+  }
+}
