12b069d3c9c13efe68b3f6837b2ddd0e231d242b
Joshua Maitoza <jmaitoza@umassd.edu>
2022-03-23 08:24:33 -0400

    finished part2

---
+++ forkExec.c
@@ -0,0 +1,26 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFSIZE 512
+
+int ForkExec(char *temp[]) {
+  pid_t pid;
+  char buffer;
+
+  pid = fork();
+  if (pid == 0) {
+    execvp(temp[0], temp);
+    exit(0);
+  }
+
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  ForkExec(argv + 1);
+  write(1, "Done\n", 5);
+  return 1;
+}
